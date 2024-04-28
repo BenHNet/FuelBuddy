6fbc6236679d18cf0b386ca730709ac9ed2ea875
Ben Hoelting <ben@htechinc.com>
2024-04-28 16:51:10 -0600

    I just cleaned up the Existing Tests

--- station_tracker/tests.py
+++ station_tracker/tests.py
@@ -1,20 +1,13 @@
 # Create your tests here.
 
+from unittest.mock import patch, MagicMock
+
+import pytest
+from django.http import HttpRequest
+from django.test import RequestFactory
 from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-from django.test import Client  # Use Django's test client
-from .models import Feedback
 
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append('/home/runner/group3-fall2023-1')  # Add this line
-os.environ['DJANGO_SETTINGS_MODULE'] = 'station_tracker.settings'
-
-
-#himaja test cases
+# himaja test cases
 class MyTestCase(TestCase):
 
     def test_user_signup_view(self):
@@ -22,19 +15,20 @@ class MyTestCase(TestCase):
         response = client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
 
-
     def test_user_login_view(self):
         client = Client()  # Create a test client
         response = client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
-   # def test_user_logout_view(self):
-      #   client = Client()  # Create a test client
-       #  response = client.get(reverse('logout'))
-        # self.assertEqual(response.status_code, 302)  # 302 is the status code for a redirect
-        # self.assertFalse(response.context['user'].is_authenticated)
+
+# def test_user_logout_view(self):
+#   client = Client()  # Create a test client
+#  response = client.get(reverse('logout'))
+# self.assertEqual(response.status_code, 302)  # 302 is the status code for a redirect
+# self.assertFalse(response.context['user'].is_authenticated)
 from django.test import TestCase, Client
 from django.urls import reverse
+
 
 class YourAppViewsTestCase(TestCase):
     def setUp(self):
@@ -66,24 +60,26 @@ class YourAppViewsTestCase(TestCase):
         self.assertTemplateUsed(response, 'payment.html')
 
 
-
-
 class FeedbackTestCases(TestCase):
 
-  def test_successful_submit(self):
-    client = Client()
-    response = client.post(reverse('feedback'), {'name': 'test', 'email': 'tzirw@example.com', 'phone': '1234567890', 'comments': 'test comments', 'gasStationAddr': '123 AnyPlace'})
-    self.assertEqual(response.status_code, 302)
+    def test_successful_submit(self):
+        client = Client()
+        response = client.post(reverse('feedback'),
+                               {'name': 'test', 'email': 'tzirw@example.com', 'phone': '1234567890',
+                                'comments': 'test comments', 'gasStationAddr': '123 AnyPlace'})
+        self.assertEqual(response.status_code, 302)
 
-  def test_missing_name(self):
-    client = Client()
-    response = client.post(reverse('feedback'), {'email': 'tzirw@example.com', 'phone': '1234567890', 'comments': 'test comments', 'gasStationAddr': '123 AnyPlace'})
-    self.assertFormError(response, 'form', 'name', 'This field is required.')
+    def test_missing_name(self):
+        client = Client()
+        response = client.post(reverse('feedback'),
+                               {'email': 'tzirw@example.com', 'phone': '1234567890', 'comments': 'test comments',
+                                'gasStationAddr': '123 AnyPlace'})
+        self.assertFormError(response, 'form', 'name', 'This field is required.')
 
 
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Customer, GasStationOwner, Gas_Station, Feedback
+
 
 class YourAppTestCase(TestCase):
 
